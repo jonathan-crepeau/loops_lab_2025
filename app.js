058fbb7b0f46cac9be1c614266969777755f4c05
jonathan-crepeau <jon.crepeau@gmail.com>
2025-01-28 20:28:31 -0800

    part 1 of while/for loops completed

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-console.log('All good under the hood...');
+// console.log('All good under the hood...');
+
+
+// SECTION - While Loops:
+
+// • Write a while loop that counts from 1 to 100:
+
+// let num = 1;
+
+// while (num <= 100) {
+//   console.log(num);
+//   num++
+// }
+
+
+// • Write a for loop that counts from 1 to 200, and for each count prints the square root of each count's number:
+
+// let num = 1;
+
+// while (num <= 200) {
+//   console.log('The square of ' + num + ' is ' + Math.sqrt(num) + '!');
+//   num++;
+// }
+
+
+// • Write a for loop that counts backwards from 1000 to 1;
+
+// let num = 1000;
+
+// while (num >= 1) {
+//   console.log('The number is ' + num + '!');
+//   num--;
+// }
+
+
+// SECTION - For Loops
+
+// • Write a for loop that counts from 999 to 9999:
+
+// for (let i = 999; i <= 9999; i++) {
+//   console.log('The number is ' + i + '.');
+//   i++
+// }
+
+
+// • Write a for loop that prints 'This is the song that never ends' to the console 100 times:
+
+// for (let i = 0; i < 100; i++) {
+//   console.log('This is the song that never ends(' + i + ').');
+// }
+
+
+// • Write a for loop that counts backwards from 1000 to 1, using i--;
+
+for (let i = 1000; i > 0; i--) {
+  console.log(i);
+}
