8ee572e60008f12638df59b86b0f22d9aae0e73c
jonathan-crepeau <jon.crepeau@gmail.com>
2025-01-31 15:15:51 -0800

    first half of function exercises completed

---
+++ functions.js
@@ -0,0 +1,108 @@
+// ANCHOR - Functions Lab & Exercises
+
+// SECTION Function Declaration vs. Definition
+
+// • The following function, multiply, is declared (function name in use) as well as defined (contains an expression):
+// • Function declarations are hoisted, meaning they can be invoked(called) ahead of where they are declared further down in a file:
+
+// console.log(multiply(2, 5));
+
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+
+// • The following function, divideTwoNumbers, is an example of a defined function (contains an expression), but not declared.
+// • A function definition is NOT hoisted, so you cannot invoke(call) the function ahead of where it is defined in the file.
+
+// const divideTwoNumbers = (num1, num2) => {
+//   return num1 / num2;
+// }
+
+// console.log(divideTwoNumbers(10, 2));
+
+
+// • Write a function, printSum, that will console.log the result of 10 + 10:
+
+// console.log(printSum());
+
+// function printSum() {
+//   return 10+10;
+// }
+
+
+// Write a function, printTriangle, that will print these pound signs to the console:
+
+// function printTriangle() {
+//   let hash = "#";
+//   for (let i = 1; i <= 5; i++ ) {
+//     console.log(hash);
+//     hash += "#";
+//   }
+// }
+
+// printTriangle();
+
+
+// • Write a function, printParameter, that takes a paramater called "input". The function should simply console.log the value of the input parameter. Invoke the function with an argument:
+
+// function printParameter(input) {
+//   console.log(input);
+// }
+
+// printParameter('Jonathan');
+
+
+// • Write a function called minusOne that takes a parameter called 'num'. Assuming the argument is a number, print the argument provided minus 1.
+
+// function minusOne(num) {
+//   if (typeof num !== 'number') {
+//     return 'Provided argument is not a number';
+//   } else {
+//     return num - 1;
+//   }
+// }
+
+// console.log(minusOne('Jonathan'));
+// console.log(minusOne(2000));
+
+
+// • Write a function, getLastElement, that takes a parameter called 'arr'. Invoke the function with an array as the argument. The function should print the last element within the array.
+
+// function getLastElement(arr) {
+//   if (Array.isArray(arr)) {
+//     return arr[arr.length - 1];
+//   } else {
+//     return 'Provided argument is not a function';
+//   }
+// }
+
+// let sampleArray = ['Luna', 'Jeremy', 'Jonathan'];
+
+// console.log(getLastElement(sampleArray));
+// console.log(getLastElement());
+
+
+// • Write a funtion, makeSentence, that takes three parameters and interpolates them into a fully formed sentence:
+
+// function makesSentence(arg1, arg2, arg3) {
+//   return arg1 + ' ' + arg2 + ' ' + arg3 + '.';
+// }
+
+// console.log(makesSentence('Our', 'cat', 'Luna'));
+
+
+// • Write a function, divideThreeNums, that takes three parameters and prints the third parameter divided by the result of the second parameter divided by the first.
+
+// function divideThreeNums(num1, num2, num3) {
+//   if (typeof num1 && typeof num2 && typeof num3 === 'number') {
+//     return num3/(num2/num1);
+//   } else {
+//     return 'Not all provided arguments are numbers.'
+//   }
+// }
+
+// console.log(divideThreeNums(2, 3, 'J'));
+// console.log(divideThreeNums(2, 3, 6));
+
+
+// • 
